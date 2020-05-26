13be919ecd111389d488490dcc187c0ef7c4d60b
AnkiitSingh <ankit.singh621998@gmail.com>
2020-05-26 12:50:13 +0530

    baseModel

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose"),
+  express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  cookieParser = require("cookie-parser"),
+  cors = require("cors");
+
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//APP MOUNT POINT
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
