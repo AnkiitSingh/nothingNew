5a057d920f35a4115406e9a0b608d07ff90e8d72
AnkiitSingh <ankit.singh621998@gmail.com>
2020-07-30 10:17:57 +0530

    email add modifications

--- controller/userController.js
+++ controller/userController.js
@@ -4,51 +4,52 @@ var expressJwt = require("express-jwt");
 
 
 exports.createUser = (req, res) => {
-    const user = new User(req.body);
-    user.save((err, user) => {
-        if(err){
-            return res.status(500).json({
-                error: "Please Include All Fields",
-            });
-        }
-        res.json({
-            email: user.email,
-            id: user._id,
-            phoneNo: user.phoneNo
-          });
-    })
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(500).json({
+        error: "include all fields or email already in use"
+      });
+    }
+    res.json({
+      email: user.email,
+      id: user._id,
+      phoneNo: user.phoneNo
+    });
+  })
 }
 
 exports.signIn = (req, res) => {
-    const { email, phoneNo } = req.body;
-    User.findOne({ email }, (err, user) => {
-      if (err || !user) {;
-        return res.status(400).json({
-          error: "USER email does not exists"
-        });
-      }
-      if (user.phoneNo.toString() !== req.body.phoneNo) {
-        return res.status(401).json({
-          error: "Phone No does not match",
-        });
-      }
-  
-      //create token
-      const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-      //put token in cookie
-      res.cookie("token", token, { expire: new Date() + 9999 });
-  
-      //send response to front end
-      const { _id, email, phoneNo, address } = user;
-      return res.json({ token, user: { _id, email, phoneNo, address } });
-    });
+  const { email, phoneNo } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      ;
+      return res.status(400).json({
+        error: "USER email does not exists"
+      });
+    }
+    if (user.phoneNo.toString() !== req.body.phoneNo) {
+      return res.status(401).json({
+        error: "Phone No does not match",
+      });
+    }
+
+    //create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    //put token in cookie
+    res.cookie("token", token, { expire: new Date() + 9999 });
+
+    //send response to front end
+    const { _id, email, phoneNo, address } = user;
+    return res.json({ token, user: { _id, email, phoneNo, address } });
+  });
 };
 
 exports.signOut = (req, res) => {
-    res.clearCookie("token");
-    res.json({
-      message: "User signout successfully"
-    });
+  res.clearCookie("token");
+  res.json({
+    message: "User signout successfully"
+  });
 };
 
 exports.isSignedIn = expressJwt({
@@ -67,45 +68,45 @@ exports.isAuthenticated = (req, res, next) => {
 };
 
 
-exports.pushCart = async(req, res) => {
-  const user = await User.find({_id: req.params.userid})
+exports.pushCart = async (req, res) => {
+  const user = await User.find({ _id: req.params.userid })
   var cartProduct = await req.params.productid;
-  if(user[0] == null){
+  if (user[0] == null) {
     return res.send("No user Found")
   }
-  if(cartProduct){
-    user[0].cart.push(cartProduct) ;
+  if (cartProduct) {
+    user[0].cart.push(cartProduct);
   }
   await user[0].save();
   return res.send(user);
 }
 
-exports.userCart = async(req, res) =>{
-  const user = await User.find({_id: req.params.userid})
+exports.userCart = async (req, res) => {
+  const user = await User.find({ _id: req.params.userid })
   const cart = user[0].cart
   res.send(cart);
 }
 
-exports.getUser = async(req, res) => {
-  const user = await User.find({_id: req.params.userId}, async function(err, person){
-    if(err){
+exports.getUser = async (req, res) => {
+  const user = await User.find({ _id: req.params.userId }, async function (err, person) {
+    if (err) {
       res.send("No user Found")
     }
-    const info = {"email":person[0].email, "phoneNo":person[0].phoneNo, "address":person[0].address};
+    const info = { "email": person[0].email, "phoneNo": person[0].phoneNo, "address": person[0].address };
     await res.send(info)
   })
 }
-exports.removeFromCart = async(req, res) => {
-  const user = await User.find({_id: req.params.userId}, async function(err, person){
-    if(err){
+exports.removeFromCart = async (req, res) => {
+  const user = await User.find({ _id: req.params.userId }, async function (err, person) {
+    if (err) {
       res.send("No user Found")
     }
-    if(req.params.no >= person[0].cart.lenght){
+    if (req.params.no >= person[0].cart.lenght) {
       res.send(("Invalid Delete request"));
     }
-    else{
+    else {
       person[0].cart.splice(req.params.no, 1);
-    } 
+    }
     await person[0].save();
     res.send(person)
   })
